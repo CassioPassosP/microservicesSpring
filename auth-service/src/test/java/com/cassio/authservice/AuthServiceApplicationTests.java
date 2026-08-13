8fb3a5f2fb5ed0ceb9acdc03ec6f48c38e1e4074
Cassio Passos <passoscassio29@gmail.com>
2026-08-13 13:21:42 -0300

    push initial structure

---
+++ auth-service/src/test/java/com/cassio/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassio.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
